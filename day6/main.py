c0f888fb4ad989f101c4a25336d3f4f02cf92c6a
ethmoses <moses.buta@yahoo.com.au>
2022-07-15 20:50:33 +1000

    day6 complete

---
+++ day6/main.py
@@ -0,0 +1,10 @@
+#Functions
+def my_function():
+    print("This is an example of a function")
+#calling the function
+my_function()
+
+#while loop
+#while loop is a loop that will continue going while a particular condition is true.
+#While something_is_true:
+#do something repeatedly
