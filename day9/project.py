d6dc7361499eaf6bd56478f8d105c7bbdace5ee9
ethmoses <moses.buta@yahoo.com.au>
2022-07-26 16:40:53 +1000

    day9 complete

--- day9/project.py
+++ day9/project.py
@@ -3,6 +3,15 @@ import os
 
 continue_bid = True
 bidding = {}
+def highest_bidder():
+    highest_bid = 0
+    winner_name = ''
+    for player in bidding:
+        if int(bidding[player]) > highest_bid:
+            highest_bid = int(bidding[player])
+            winner_name = player
+    print(f"The winner is {winner_name} with a bid of ${highest_bid}")
+
 print(logo)
 print("Welcome to the secret auction program.")
 while continue_bid:
@@ -11,13 +20,7 @@ while continue_bid:
     bidding[name] = amount
     other_bidders = input("Are there any other bidders? Type 'yes' or 'no'.: ").lower()
     if other_bidders == 'no':
-        highest_bid = 0
-        winner_name = ''
-        for player in bidding:
-            if int(bidding[player]) > highest_bid:
-                highest_bid = int(bidding[player])
-                winner_name = player
-        print(f"The winner is {winner_name} with a bid of ${highest_bid}")
+        highest_bidder()
         continue_bid = False
     else:
         os.system('clear')
