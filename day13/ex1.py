971e08f704d7acf09c3c5a75fa951f6061164930
ethmoses <moses.buta@yahoo.com.au>
2022-07-29 08:12:37 +1000

    Day13 complete

---
+++ day13/ex1.py
@@ -0,0 +1,15 @@
+# Instructions
+# Read this the code in main.py
+# Spot the problems 🐞.
+# Modify the code to fix the program.
+# Fix the code so that it works and passes the tests when you submit.
+#
+# Hint
+# Review the previous lesson and go through the 10 steps to tackle these debugging problems.
+
+number = int(input("Which number do you want to check?: "))
+
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
