6e5715d7ce154438e7d8cc019d916a3cbc1bea01
ethmoses <moses.buta@yahoo.com.au>
2022-07-29 11:58:23 +1000

    day14 complete

--- day14/project.py
+++ day14/project.py
@@ -14,26 +14,40 @@ def generate_random_individuals():
         return random
 
 def display_opitions(p1, p2):
-    if score > 0: print(f"You're right! Current score: {score}")
     print(f"Compare A: {p1['name']}, a {p1['description']}, from {p1['country']}")
     print(vs)
     print(f"Against B: {p2['name']}, a {p2['description']}, from {p2['country']}")
 
+def highest_individual():
+    if opition_a['follower_count'] > opition_b['follower_count']:
+        return 'A'
+    elif opition_b['follower_count'] > opition_a['follower_count']:
+        return 'B'
+def check_win(user_choice):
+    global score
+    if user_choice == highest_individual():
+        return 1
+    else:
+        return 0
 
 while continue_palying:
-    os.system("clear")
     print(logo)
-    
     display_opitions(opition_a,opition_b)
-    user_choice = input("Who has more followers? Type 'A' or 'B': ")
-    if opition_a['follower_count'] > opition_b['follower_count'] and user_choice == "A":
-        score +=1
-        opition_a = opition_b
-        opition_b = generate_random_individuals()
-    elif opition_b['follower_count'] > opition_a['follower_count'] and user_choice == "B":
-        score +=1
-        opition_a = opition_b
-        opition_b = generate_random_individuals()
-    else:
-        print(f"Sorry, that's wrong. Final score: {score}")
-        continue_palying = False
+    play_game = True
+    while play_game:
+        user_choice = input("Who has more followers? Type 'A' or 'B': ")
+        if check_win(user_choice) == 1:
+            os.system('clear')
+            score +=1
+            print(logo)
+            print(f"You're right! Current score: {score}")
+            opition_a = opition_b
+            opition_b = generate_random_individuals()
+            display_opitions(opition_a,opition_b)
+        else:
+            os.system('clear')
+            print(logo)
+            print(f"Sorry, that's wrong. Final score: {score}")
+            play_game = False
+    continue_palying = False
+    
